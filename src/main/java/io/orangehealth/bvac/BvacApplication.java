de16f88dd187cad81ef03c50bd55a535a495b6e8
syltare <rafael.rodrigues@tuta.io>
2021-03-09 18:33:49 +0100

    Initial commit

---
+++ src/main/java/io/orangehealth/bvac/BvacApplication.java
@@ -0,0 +1,13 @@
+package io.orangehealth.bvac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BvacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/orangehealth/bvac/BvacApplicationTests.java
@@ -0,0 +1,13 @@
+package io.orangehealth.bvac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
